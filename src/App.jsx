5ec08a63b799603f25e7445fcca4da6b9e69a9c1
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-09-27 22:09:30 +0100

    Ottergram Project Creation

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import "./App.css";
+import Barry from "./assets/otters/otter1.jpg";
+import Robin from "./assets/otters/otter2.jpg";
+import Maurice from "./assets/otters/otter3.jpg";
+import Lesley from "./assets/otters/otter4.jpg";
+import Barbara from "./assets/otters/otter5.jpg";
+
+function App() {
+  return (
+    <div>
+      <header className="header-component">
+        <h1>Ottergram</h1>
+      </header>
+
+      <ul className="post-list">
+        <li className="post-component">
+          <button>
+            <img src={Barry} alt="Barry" />
+            <p>Barry</p>
+          </button>
+        </li>
+        <li className="post-component">
+          <button>
+            <img src={Robin} alt="Robin" />
+            <p>Robin</p>
+          </button>
+        </li>
+        <li className="post-component">
+          <button>
+            <img src={Maurice} alt="Maurice" />
+            <p>Maurice</p>
+          </button>
+        </li>
+        <li className="post-component">
+          <button>
+            <img src={Lesley} alt="Barry" />
+            <p>Barry</p>
+          </button>
+        </li>
+        <li className="post-component">
+          <button>
+            <img src={Barbara} alt="Barbara" />
+            <p>Barbara</p>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
